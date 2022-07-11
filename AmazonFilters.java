a6cde9ffdc1e8e03e550e8c046ec1f0f214bd001
GaneshKrishnanb <96409425+GaneshKrishnanb@users.noreply.github.com>
2022-07-11 09:10:19 +0530

    Update AmazonFilters.java

--- AmazonFilters.java
+++ AmazonFilters.java
@@ -22,7 +22,6 @@ public static void main(String[] args) {
 	 driver.findElement(By.xpath("//li[@id=\"p_90/6741118031\"]/span/a/div/label/i")).click();
 	 System.out.println(driver.findElement(By.xpath("//span [@class = 'a-size-base a-color-base a-text-bold']")).getText());
 	 driver.findElement(By.linkText("Smartphones")).click();
-	 //System.out.println(driver.findElement(By.className("a-size-base a-color-base")).getText());
 	 driver.findElement(By.xpath("//i[@class='a-icon a-icon-star-medium a-star-medium-4']")).click();
 	 System.out.println(driver.findElement(By.xpath("//div[@id ='p_72-title']/span")).getText());
 	 driver.findElement(By.xpath("//li[@id ='p_36/1318507031']/span/a/span")).click();
